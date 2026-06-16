00603f8b158d8fa65411213568f6059c36b57283
mr9 asif <asifalibd002@gmail.com>
2026-06-16 11:26:28 +0600

    fix toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
        <AuthProvider>
          <App />
+             <Toaster position="top-right" />
        </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
