99e4ce1f27ae0d8ee388403ba822a0ea99bb45c7
mr9asif <asifalibd002@gmail.com>
2026-08-10 10:59:35 +0600

    fix

--- backend/src/util/cookieOption.ts
+++ backend/src/util/cookieOption.ts
@@ -2,7 +2,7 @@ import type { CookieOptions } from "express";
 
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
